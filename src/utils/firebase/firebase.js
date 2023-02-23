7f6330fbfa59b8d2bdd83f0714cbeb1939f10d03
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2023-02-23 18:14:14 +0000

    Added authentication for firebase

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -5,6 +5,12 @@ import {
 	signInWithPopup,
 	GoogleAuthProvider,
 } from 'firebase/auth';
+import {
+	getFirestore,
+	doc,
+	getDoc,
+	setDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyAx740mmC4dLQpzcyDpClZC1DbPTvxNpX8',
@@ -22,6 +28,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Auth
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
@@ -31,3 +38,37 @@ provider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
 	signInWithPopup(auth, provider);
+
+// Database - Auth
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	// Check if user data exists
+
+	// If user data does not exist
+	if (!userSnapshot.exists()) {
+		// Create / Set the document with the data from userAuth in my collection
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log('Error creating user', error.message);
+		}
+	}
+
+	return userDocRef;
+};
