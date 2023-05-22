aa1c8e034b7773635d88b18bc4f72b034daac278
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2023-05-22 16:34:06 +0100

    Added home button to order success page

--- src/routes/OrderSuccess/OrderSuccess.jsx
+++ src/routes/OrderSuccess/OrderSuccess.jsx
@@ -1,3 +1,8 @@
+import { Link } from 'react-router-dom';
+
+// Components
+import Button from '../../components/Button/Button';
+
 // Styles
 import './orderSuccess.scss';
 
@@ -7,6 +12,9 @@ const OrderSuccess = () => {
 			<h1 className='title'>Order Successful! 🥳 </h1>
 			<p className='text'>Thanks for your order!</p>
 			<p className='small'>Don't worry, you haven't really ordered anything!</p>
+			<Link to='/'>
+				<Button buttonType='inverted'>Home</Button>
+			</Link>
 		</div>
 	);
 };
